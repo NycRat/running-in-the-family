165f8ce9b43f009733d52f78f130d04932ea2a40
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2023-12-18 20:54:40 -0800

    style updates

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const options = {
+const rightOptions = {
   color: "var(--foreground-2)",
+  sourcePosition: "middleLeft",
+  destinationPosition: "middleRight",
+};
+
+const leftOptions = {
+  color: "var(--foreground-2)",
+  sourcePosition: "middleRight",
+  destinationPosition: "middleLeft",
 };
 
 const arrows = [
-  arrowLine("#quote0", "#p5", options),
-  arrowLine("#quote1", "#p6", options),
-  arrowLine("#quote2", "#p6", options),
-  arrowLine("#quote3", "#p8", options),
-  arrowLine("#quote4", "#p9", options),
-  arrowLine("#quote5", "#p9", options),
-  arrowLine("#quote111", "#p111", options),
+  arrowLine("#quote0", "#p5", rightOptions),
+  arrowLine("#quote1", "#p6", rightOptions),
+  arrowLine("#quote2", "#p6", rightOptions),
+  arrowLine("#quote3", "#p8", rightOptions),
+  arrowLine("#quote4", "#p9", rightOptions),
+  arrowLine("#quote5", "#p9", rightOptions),
+  arrowLine("#quote111", "#p111", leftOptions),
 ];
 
 window.addEventListener("resize", () => {
