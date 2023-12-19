f1b39d000160516e0fd1ed87590c789caf663b41
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2023-12-18 19:22:04 -0800

    entry 1

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const options = {
   color: "var(--foreground-2)",
 };
 
-const arrow = arrowLine("#quote1", "#p5", options);
-const arrow1 = arrowLine("#quote2", "#p9", options);
+const arrows = [
+  arrowLine("#quote0", "#p5", options),
+  arrowLine("#quote1", "#p6", options),
+  arrowLine("#quote2", "#p6", options),
+  arrowLine("#quote3", "#p8", options),
+  arrowLine("#quote4", "#p9", options),
+  arrowLine("#quote5", "#p9", options),
+  arrowLine("#quote111", "#p111", options),
+];
 
 window.addEventListener("resize", () => {
-  arrow.update({});
-  arrow1.update({});
+  arrows.forEach((x) => x.update({}));
+});
+
+window.addEventListener("load", () => {
+  arrows.forEach((x) => x.update({}));
 });
